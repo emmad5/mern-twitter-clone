c4764588133ea6f7231e4590e70f7f65eb4a216a
Emma Deas <emmadeas9@gmail.com>
2018-12-27 21:01:38 -0800

    create mern twitter clone

---
+++ app.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
