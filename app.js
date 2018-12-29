2fcd62dc8c0f7d7aa07d30307ab8897491152669
Emma Deas <emmadeas9@gmail.com>
2018-12-28 19:59:20 -0800

    help me

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const User = require('./models/User');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
 
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-
+    
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -31,6 +33,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
