461259d95b04dedd0600b5ff1b238a7ef2f0030c
Emma Deas <emmadeas9@gmail.com>
2018-12-28 19:35:01 -0800

    fix bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
 
-// serviceWorker.unregister();
+serviceWorker.unregister();
 
 
 document.addEventListener('DOMContentLoaded', () => {
